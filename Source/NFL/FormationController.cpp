5caada02542b26205a627a3565b43e4e826013fa
Znasif <1305030.nz@ugrad.cse.buet.ac.bd>
2023-09-07 19:36:47 -0700

    only animation left
    Signed-off-by: Znasif <1305030.nz@ugrad.cse.buet.ac.bd>

--- Source/NFL/FormationController.cpp
+++ Source/NFL/FormationController.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "FormationController.h"
 
 // Sets default values
@@ -15,13 +14,254 @@ AFormationController::AFormationController()
 void AFormationController::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
 void AFormationController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+bool AFormationController::LoadTextFromFile(FString FileName, TArray<FString>& TextArray)
+{
+
+	if (!FPlatformFileManager::Get().GetPlatformFile().FileExists(*FileName))
+	{
+		//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, TEXT("Could not Find File"));
+		return false;
+	}
+	else
+	{
+		// Convert filepath to character array and save to array
+		const TCHAR* FILEPATH = *FileName;
+		return FFileHelper::LoadFileToStringArray(TextArray, *FileName);
+	}
+}
+
+bool AFormationController::SaveArrayText(FString SaveDirectory, FString FileName, TArray<FString> SaveText, bool AllowOverwriting = false)
+{
+	// Set complete file path
+	SaveDirectory += "\\";
+	SaveDirectory += FileName;
+
+	if (!AllowOverwriting)
+	{
+		if (FPlatformFileManager::Get().GetPlatformFile().FileExists(*SaveDirectory))
+		{
+			return false;
+		}
+	}
+
+	FPlatformFileManager::Get().GetPlatformFile().DeleteFile(*SaveDirectory);
+	FString FinalString = "";
+	for (FString& Each : SaveText)
+	{
+		FinalString += Each;
+		FinalString += LINE_TERMINATOR;
+	}
+
+	return FFileHelper::SaveStringToFile(FinalString, *SaveDirectory);
 
 }
 
+bool AFormationController::DeleteTextFile(FString SaveDirectory, FString FileName)
+{
+	// Set complete file path
+	SaveDirectory += "\\";
+	SaveDirectory += FileName;
+
+	return FPlatformFileManager::Get().GetPlatformFile().DeleteFile(*SaveDirectory);
+}
+
+void AFormationController::LoadFootballCSV(FString SaveDirectory, FFootballArray& football)
+{
+	//GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Football: %s"), *(SaveDirectory)));
+	TArray<FString> loadfoot;
+	LoadTextFromFile(SaveDirectory, loadfoot);
+	TArray<float> onlyLook = { 3, 4, 18, 19};
+	TArray<float> readfloat = { 0, 0, 0, 0 };
+	FFootballArray temp;
+	for (int i = 1; i < loadfoot.Num(); i++)
+	{
+		FString left = "";
+		int flag = 0;
+		FString br = loadfoot[i];
+		int j = 0, k=0;
+
+		while (j < br.Len()) {
+			if (br[j] == ',') {
+				if (flag<4 && k==onlyLook[flag]) {
+					readfloat[flag] = FCString::Atof(*left);
+					flag++;
+					//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Float : %s"), *left));
+				}
+				else if (k == 8) {
+					const UEnum* EnumPtr = FindObject<UEnum>(ANY_PACKAGE, TEXT("ENFLEvent"), true);
+					if (left.Contains(TEXT(";"))) {
+						TArray<FString> eventarr;
+						left.ParseIntoArray(eventarr, TEXT(";"), true);
+						for (int ev = 1; ev < eventarr.Num(); ev++) {
+							//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Event : %s"), *eventarr[ev]));
+							int32 Index = EnumPtr->GetIndexByName(FName(*eventarr[ev]));
+							if (Index != INDEX_NONE) {
+								ENFLEvent Status = static_cast<ENFLEvent>(EnumPtr->GetValueByIndex(Index));
+								temp.FFootballEventArray.Add(Status, i);
+							}
+						}
+					}
+					else {
+						//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Event : %s"), *left));
+						int32 Index = EnumPtr->GetIndexByName(FName(*left));
+						if (Index != INDEX_NONE) {
+							ENFLEvent Status = static_cast<ENFLEvent>(EnumPtr->GetValueByIndex(Index));
+							temp.FFootballEventArray.Add(Status, i);
+						}
+					}
+					
+				}
+				left = "";
+				k++;
+			}
+			else {
+				left += br[j];
+			}
+			j++;
+		}
+		if (flag < 4 && k == onlyLook[flag]) {
+			readfloat[flag] = FCString::Atof(*left);
+			flag++;
+			//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Float : %s"), *left));
+		}
+		temp.FFootballSpeedArray.Add(readfloat[0]);
+		temp.FFootballAccelArray.Add(readfloat[1]);
+		temp.FFootballLocationArray.Add(FTransform(FVector(readfloat[2], readfloat[3], 0.0f)));
+	}
+	football = temp;
+}
+
+void AFormationController::LoadPlayerCSV(FString SaveDirectory, FPlayerArray& someplayer, int& penaltyJerseyNumber, TArray<ENFLPenalty>& penaltyType)
+{
+	FPlayerArray temp;
+	//GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Player: %s"), *(SaveDirectory)));
+	TArray<FString> loadplayer;
+	
+	LoadTextFromFile(SaveDirectory, loadplayer);
+	
+	FString FileName = FPaths::GetCleanFilename(SaveDirectory);
+	TArray<FString> Parts;
+	FileName.ParseIntoArray(Parts, TEXT("_"), true);
+
+	// Now Parts[0] contains "17", Parts[1] contains "WR", and Parts[2] contains "AM-POL.csv"
+	temp.playerJerseyNumber = FCString::Atoi(*Parts[0]); // "17"
+	const UEnum* EnumPtr = FindObject<UEnum>(ANY_PACKAGE, TEXT("ENFLPosition"), true);
+	int32 Index = EnumPtr->GetIndexByName(FName(*Parts[1]));
+	if (Index != INDEX_NONE) {
+		temp.playerPosition = static_cast<ENFLPosition>(EnumPtr->GetValueByIndex(Index));
+	}
+	if (Parts.Num() == 3) {
+		FString ThirdPart = Parts[2].LeftChop(4); // "AM-POL"
+		Parts.Empty();
+		if (ThirdPart.Len() == 0) {
+
+		}
+		else {
+			ThirdPart.ParseIntoArray(Parts, TEXT("-"), true);
+			for (int i = 0; i < Parts.Num(); i++) {
+				const UEnum* EnumPtr1 = FindObject<UEnum>(ANY_PACKAGE, TEXT("ENFLPenalty"), true);
+				Index = EnumPtr1->GetIndexByName(FName(*Parts[i]));
+				if (Index != INDEX_NONE) {
+					penaltyType.Add(static_cast<ENFLPenalty>(EnumPtr1->GetValueByIndex(Index)));
+					penaltyJerseyNumber = temp.playerJerseyNumber;
+				}
+			}
+		}
+	}
+	
+	
+	TArray<float> readfloat = { 0, 0, 0, 0, 0 };
+	
+	for (int i = 1; i < loadplayer.Num(); i++)
+	{
+		FString left = "";
+		FString br = loadplayer[i];
+		int j = 0, k = 0;
+
+		while (j < br.Len()) {
+			if (br[j] == ',') {
+				if (k<4) {
+					readfloat[k] = FCString::Atof(*left);
+					//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Float : %s"), *left));
+				}
+				left = "";
+				k++;
+			}
+			else {
+				left += br[j];
+			}
+			j++;
+		}
+		if (k == 4) {
+			readfloat[k] = FCString::Atof(*left);
+			k++;
+			//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Float : %s"), *left));
+		}
+		temp.FPlayerLocationArray.Add(FTransform(FVector(readfloat[0], readfloat[1], 0.0f)));
+		temp.FPlayerSpeedArray.Add(readfloat[2]);
+		temp.FPlayerAccelArray.Add(readfloat[3]);
+		temp.FPlayerHeadingArray.Add(readfloat[4]);
+		
+	}
+	someplayer = temp;
+}
+
+
+
+
+void AFormationController::readRandomFormation(int& penaltyJerseyNumber, bool& penaltyHome, TArray<ENFLPenalty>& penaltyType, TArray<FFootballArray>& Football, TArray<FPlayerArray>& HomeTeam, TArray<FPlayerArray>& AwayTeam){
+	TArray<FString> penalties = {"WED", "TRPd", "FCI", "IDP", "ILH", "UOHd", "KCI", "HC", "RNK", "OFK", "CHB", "UNSd", "ILF", "TRP", "UNRd", "DOF", "UNR", "ITK", "OH", "POK", "UNS", "IDT", "IBW", "FMM"};
+	FString Directory = UKismetSystemLibrary::GetProjectSavedDirectory() + "inputs/";// +TEXT("*");
+	
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	TArray<FString> all_plays;
+	LoadTextFromFile(Directory+ "name.csv", all_plays);
+	int32 RandomIndex = FMath::RandRange(1, all_plays.Num() - 1);
+	FString gp = all_plays[RandomIndex];
+	TArray<ENFLPenalty> all_penalties;
+
+	//GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Outside: %s"), *(Directory+gp)));
+	if (PlatformFile.DirectoryExists(*(Directory + gp)))
+	{
+		FString ext = ".csv";
+		FFootballArray randfoot;
+		LoadFootballCSV(Directory + gp + "/football.csv", randfoot);
+		Football.Add(randfoot);
+		
+		
+		TArray<FString> Homefolder;
+		PlatformFile.FindFiles(Homefolder, *(Directory+gp+"/home/"), *ext);
+		//GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Inside: %s"), *(Directory + gp + "/home/")));
+		for (size_t i = 0; i < Homefolder.Num(); i++)
+		{
+			FPlayerArray someplayer;
+			LoadPlayerCSV(Homefolder[i], someplayer, penaltyJerseyNumber, all_penalties);
+			HomeTeam.Add(someplayer);
+		}
+		if (all_penalties.Num() > 0) {
+			penaltyHome = true;
+		}
+		else {
+			penaltyHome = false;
+		}
+
+		TArray<FString> Awayfolder;
+		PlatformFile.FindFiles(Awayfolder, *(Directory + gp + "/away/"), *ext);
+		//GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Inside: %s"), *(Directory + gp + "/away/")));
+		for (size_t i = 0; i < Awayfolder.Num(); i++)
+		{
+			FPlayerArray someplayer;
+			LoadPlayerCSV(Awayfolder[i], someplayer, penaltyJerseyNumber, all_penalties);
+			AwayTeam.Add(someplayer);
+		}
+		penaltyType = all_penalties;
+	}
+}
--- Source/NFL/FormationController.h
+++ Source/NFL/FormationController.h
@@ -4,7 +4,197 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GenericPlatform/GenericPlatformFile.h"
+#include "Misc/Paths.h"
+#include "Kismet/KismetSystemLibrary.h"
 #include "FormationController.generated.h"
+
+UENUM(BlueprintType)
+enum class ENFLEvent : uint8
+{
+	kickoff UMETA(DisplayName = "kickoff"),
+	touchback UMETA(DisplayName = "touchback"),
+	ball_snap UMETA(DisplayName = "ball_snap"),
+	field_goal_attempt UMETA(DisplayName = "field_goal_attempt"),
+	field_goal UMETA(DisplayName = "field_goal"),
+	kick_received UMETA(DisplayName = "kick_received"),
+	first_contact UMETA(DisplayName = "first_contact"),
+	tackle UMETA(DisplayName = "tackle"),
+	punt UMETA(DisplayName = "punt"),
+	punt_land UMETA(DisplayName = "punt_land"),
+	punt_downed UMETA(DisplayName = "punt_downed"),
+	out_of_bounds UMETA(DisplayName = "out_of_bounds"),
+	extra_point_attempt UMETA(DisplayName = "extra_point_attempt"),
+	extra_point UMETA(DisplayName = "extra_point"),
+	fair_catch UMETA(DisplayName = "fair_catch"),
+	punt_received UMETA(DisplayName = "punt_received"),
+	touchdown UMETA(DisplayName = "touchdown"),
+	kickoff_land UMETA(DisplayName = "kickoff_land"),
+	punt_muffed UMETA(DisplayName = "punt_muffed"),
+	fumble_offense_recovered UMETA(DisplayName = "fumble_offense_recovered"),
+	autoevent_kickoff UMETA(DisplayName = "autoevent_kickoff"),
+	onside_kick UMETA(DisplayName = "onside_kick"),
+	kick_recovered UMETA(DisplayName = "kick_recovered"),
+	extra_point_missed UMETA(DisplayName = "extra_point_missed"),
+	punt_play UMETA(DisplayName = "punt_play"),
+	line_set UMETA(DisplayName = "line_set"),
+	fumble UMETA(DisplayName = "fumble"),
+	fumble_defense_recovered UMETA(DisplayName = "fumble_defense_recovered"),
+	free_kick UMETA(DisplayName = "free_kick"),
+	field_goal_missed UMETA(DisplayName = "field_goal_missed"),
+	field_goal_blocked UMETA(DisplayName = "field_goal_blocked"),
+	pass_forward UMETA(DisplayName = "pass_forward"),
+	pass_arrived UMETA(DisplayName = "pass_arrived"),
+	pass_outcome_caught UMETA(DisplayName = "pass_outcome_caught"),
+	lateral UMETA(DisplayName = "lateral"),
+	run UMETA(DisplayName = "run"),
+	pass_shovel UMETA(DisplayName = "pass_shovel"),
+	field_goal_play UMETA(DisplayName = "field_goal_play"),
+	extra_point_blocked UMETA(DisplayName = "extra_point_blocked"),
+	penalty_flag UMETA(DisplayName = "penalty_flag"),
+	man_in_motion UMETA(DisplayName = "man_in_motion"),
+	punt_fake UMETA(DisplayName = "punt_fake"),
+	punt_blocked UMETA(DisplayName = "punt_blocked"),
+	drop_kick UMETA(DisplayName = "drop_kick"),
+	field_goal_fake UMETA(DisplayName = "field_goal_fake"),
+	pass_outcome_incomplete UMETA(DisplayName = "pass_outcome_incomplete"),
+	safety UMETA(DisplayName = "safety"),
+	snap_direct UMETA(DisplayName = "snap_direct"),
+	handoff UMETA(DisplayName = "handoff"),
+	kickoff_play UMETA(DisplayName = "kickoff_play"),
+	qb_sack UMETA(DisplayName = "qb_sack"),
+	shift UMETA(DisplayName = "shift"),
+	huddle_break_offense UMETA(DisplayName = "huddle_break_offense"),
+	huddle_start_offense UMETA(DisplayName = "huddle_start_offense"),
+	two_point_conversion UMETA(DisplayName = "two_point_conversion"),
+	field_goal_miseed UMETA(DisplayName = "field_goal_miseed")
+};
+
+
+UENUM(BlueprintType)
+enum class ENFLPosition : uint8
+{
+	C UMETA(DisplayName = "C"),
+	CB UMETA(DisplayName = "CB"),
+	DB UMETA(DisplayName = "DB"),
+	DE UMETA(DisplayName = "DE"),
+	DL UMETA(DisplayName = "DL"),
+	DT UMETA(DisplayName = "DT"),
+	FB UMETA(DisplayName = "FB"),
+	FS UMETA(DisplayName = "FS"),
+	G UMETA(DisplayName = "G"),
+	HB UMETA(DisplayName = "HB"),
+	ILB UMETA(DisplayName = "ILB"),
+	K UMETA(DisplayName = "K"),
+	LB UMETA(DisplayName = "LB"),
+	LS UMETA(DisplayName = "LS"),
+	MLB UMETA(DisplayName = "MLB"),
+	NT UMETA(DisplayName = "NT"),
+	OG UMETA(DisplayName = "OG"),
+	OLB UMETA(DisplayName = "OLB"),
+	OT UMETA(DisplayName = "OT"),
+	P UMETA(DisplayName = "P"),
+	QB UMETA(DisplayName = "QB"),
+	RB UMETA(DisplayName = "RB"),
+	S UMETA(DisplayName = "S"),
+	SAF UMETA(DisplayName = "SAF"),
+	SS UMETA(DisplayName = "SS"),
+	T UMETA(DisplayName = "T"),
+	TE UMETA(DisplayName = "TE"),
+	WR UMETA(DisplayName = "WR")
+};
+
+UENUM(BlueprintType)
+enum class ENFLPenalty : uint8
+{
+	BAT UMETA(DisplayName = "BAT"),
+	CHB UMETA(DisplayName = "CHB"),
+	DOF UMETA(DisplayName = "DOF"),
+	DPI UMETA(DisplayName = "DPI"),
+	DSQd UMETA(DisplayName = "DSQd"),
+	DTM UMETA(DisplayName = "DTM"),
+	FCI UMETA(DisplayName = "FCI"),
+	FMM UMETA(DisplayName = "FMM"),
+	HC UMETA(DisplayName = "HC"),
+	IBB UMETA(DisplayName = "IBB"),
+	IBW UMETA(DisplayName = "IBW"),
+	IDK UMETA(DisplayName = "IDK"),
+	IDP UMETA(DisplayName = "IDP"),
+	IDT UMETA(DisplayName = "IDT"),
+	IFC UMETA(DisplayName = "IFC"),
+	ILF UMETA(DisplayName = "ILF"),
+	ILH UMETA(DisplayName = "ILH"),
+	ILHd UMETA(DisplayName = "ILHd"),
+	ILM UMETA(DisplayName = "ILM"),
+	ING UMETA(DisplayName = "ING"),
+	ISH UMETA(DisplayName = "ISH"),
+	ITK UMETA(DisplayName = "ITK"),
+	KCI UMETA(DisplayName = "KCI"),
+	KOB UMETA(DisplayName = "KOB"),
+	LBL UMETA(DisplayName = "LBL"),
+	LEA UMETA(DisplayName = "LEA"),
+	LEV UMETA(DisplayName = "LEV"),
+	OFK UMETA(DisplayName = "OFK"),
+	OH UMETA(DisplayName = "OH"),
+	POK UMETA(DisplayName = "POK"),
+	RNK UMETA(DisplayName = "RNK"),
+	RRK UMETA(DisplayName = "RRK"),
+	TAU UMETA(DisplayName = "TAU"),
+	TAUd UMETA(DisplayName = "TAUd"),
+	TRP UMETA(DisplayName = "TRP"),
+	TRPd UMETA(DisplayName = "TRPd"),	
+	UNR UMETA(DisplayName = "UNR"),
+	UNRd UMETA(DisplayName = "UNRd"),
+	UNS UMETA(DisplayName = "UNS"),
+	UNSd UMETA(DisplayName = "UNSd"),
+	UOHd UMETA(DisplayName = "UOHd"),
+	WED UMETA(DisplayName = "WED")
+};
+
+USTRUCT(BlueprintType)
+struct FPlayerArray
+{
+	GENERATED_BODY()
+
+	// The following member variable will be accessible by Blueprint Graphs:
+	UPROPERTY(BlueprintReadWrite, Category = "Player Location Array")
+	TArray<FTransform> FPlayerLocationArray;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Player Location Array")
+	TArray<float> FPlayerHeadingArray;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Player Location Array")
+	TArray<float> FPlayerSpeedArray;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Player Location Array")
+	TArray<float> FPlayerAccelArray;
+	
+	UPROPERTY(BlueprintReadWrite, Category = "Player Information")
+	int playerJerseyNumber;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Player Information")
+	ENFLPosition playerPosition;
+};
+
+USTRUCT(BlueprintType)
+struct FFootballArray
+{
+	GENERATED_BODY()
+
+	// The following member variable will be accessible by Blueprint Graphs:
+	UPROPERTY(BlueprintReadWrite, Category = "Football Location Array")
+	TArray<FTransform> FFootballLocationArray;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Football Location Array")
+	TArray<float> FFootballSpeedArray;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Football Location Array")
+	TArray<float> FFootballAccelArray;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Football Event Array")
+	TMap<ENFLEvent, int> FFootballEventArray;
+};
+
 
 UCLASS()
 class NFL_API AFormationController : public AActor
@@ -22,5 +212,21 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	UFUNCTION(BlueprintCallable, Category = "Fileread", meta = (Keywords = "Load"))
+	static bool LoadTextFromFile(FString FileName, TArray<FString>& TextArray);
 
+	UFUNCTION(BlueprintCallable, Category = "Fileread", meta = (Keywords = "Save"))
+	static bool SaveArrayText(FString SaveDirectory, FString FileName, TArray<FString> SaveText, bool AllowOverwriting);
+
+	UFUNCTION(BlueprintCallable, Category = "Fileread", meta = (Keywords = "Save"))
+	static bool DeleteTextFile(FString SaveDirectory, FString FileName);
+
+	UFUNCTION(BlueprintCallable, Category = "Fileread", meta = (Keywords = "Save"))
+	void LoadFootballCSV(FString SaveDirectory, FFootballArray& football);
+
+	UFUNCTION(BlueprintCallable, Category = "Fileread", meta = (Keywords = "Save"))
+	void LoadPlayerCSV(FString SaveDirectory, FPlayerArray& someplayer, int& penaltyJerseyNumber, TArray<ENFLPenalty>& penaltyType);
+
+	UFUNCTION(BlueprintCallable, Category = "Fileread", meta = (Keywords = "Save"))
+	void readRandomFormation(int& penaltyJerseyNumber, bool& penaltyHome, TArray<ENFLPenalty>& penaltyType, TArray<FFootballArray>& Football, TArray<FPlayerArray>& HomeTeam, TArray<FPlayerArray>& AwayTeam);
 };
